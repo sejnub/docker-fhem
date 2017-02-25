e46b59f71b5e43659180964d1ee628e66c2d41dc
Heiner Bunjes <heiner@bunjes.net>
2017-02-25 16:31:37 +0100

    Update configure-fhem.sh

--- entrypoint/configure-fhem.sh
+++ entrypoint/configure-fhem.sh
@@ -5,3 +5,6 @@ sed -i -e "s/<fhem_web_pass_1>/$HB_FHEMWEB_PASSWORD/" /opt/fhem/fhem.cfg
 
 sed -i -e "s/<mqtt_user>/$HB_RABBITMQ_DEFAULT_USER/"  /opt/fhem/fhem.cfg
 sed -i -e "s/<mqtt_pass>/$HB_RABBITMQ_DEFAULT_PASS/"  /opt/fhem/fhem.cfg
+
+mv fhem.cfg     fhem.cfg.orig
+cp fhem.cfg.new fhem.cfg
